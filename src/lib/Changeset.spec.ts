e76f6d03501181e6ec6590ff74e6925b94df4538
Kevin Cartwright <kevin.cartwright@trademe.co.nz>
2018-11-07 12:14:16 +1300

    cleanup changeset spec

--- src/lib/Changeset.spec.ts
+++ src/lib/Changeset.spec.ts
@@ -10,10 +10,7 @@ test('Changeset exists', () => {
 
 test('Should create a warden file array for specified paths', () => {
     const cwd = process.cwd();
-    const pathOne = `${cwd}\\test\\wardenPath\\foo\\bar`;
-    const pathTwo = `${cwd}\\test\\wardenPathToo\\foo\\bar`;
-    const testPathsArray = [pathOne, pathTwo];
-
+  
     const pathOneFile = `${cwd}\\test\\wardenPath\\.warden`;
     const pathTwoFile = `${cwd}\\test\\wardenPathToo\\.warden`;
 
@@ -21,20 +18,11 @@ test('Should create a warden file array for specified paths', () => {
     const wardenFileTwo = new WardenFile(pathTwoFile);
     const expectArray = [wardenFileOne, wardenFileTwo];
 
+    const testPathsArray = [
+        `${cwd}\\test\\wardenPath\\foo\\bar`, 
+        `${cwd}\\test\\wardenPathToo\\foo\\bar`
+    ];
     const changeSet = new Changeset(testPathsArray);
 
     expect(changeSet.wardenFileArray).toEqual(expectArray);
 });
-
-// test('should create a warden file map for specified paths', () => {
-//     const cwd = process.cwd();
-//     const pathOne = `${cwd}\\test\\wardenPath\\foo\\bar`;
-//     const pathTwo = `${cwd}\\test\\wardenPathToo\\foo\\bar`;
-//     const testPathsArray = [pathOne, pathTwo];
-
-//     const expectedWardenMap = new Map();
-
-//     const changeSet = new Changeset(testPathsArray);
-    
-//     expect(changeSet).toEqual(expectedWardenMap);
-// });
